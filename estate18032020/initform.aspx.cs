c392dd479752eeaa2a86b425a983e0a5f3342e60
mario wakeham <mariowakeham@outlook.com>
2020-03-18 09:16:06 +0000

    Add project files.

---
+++ estate18032020/initform.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace estate18032020
+{
+    public partial class initform : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string hel = "hellow world";
+            Button1.Text = hel;
+        }
+    }
+}
